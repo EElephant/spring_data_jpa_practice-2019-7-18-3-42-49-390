9484381c2b448cc0b1c224c36428c5f854fb8f3f
Ethan <ethan.huang@oocl.com>
2019-07-21 20:01:28 +0800

    Feat: init three prosecutor info for PROSECUTOR table

---
+++ src/main/resources/db/migration/V4__insert_data_for_PROSECUTOR.sql
@@ -0,0 +1,5 @@
+alter table PROSECUTOR alter column BIRTHDAY varchar(50);
+
+insert into PROSECUTOR values (0,'张三', 18,'男','2001/10/20' , '001','0');
+insert into PROSECUTOR values (1,'李四', 28,'女','1982/10/20' , '002','10');
+insert into PROSECUTOR values (2,'王五', 58,'男','1972/10/20' , '003','15');
